31f218c3b062e341e0b91090eadecf4500853e87
React Learning <amwilson2019@gmail.com>
2019-07-16 19:14:09 -0400

    few more buttons to add and styling

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,20 @@
 import React from "react";
-
 //import any components needed
-
+import {useState} from 'react';
+import NumberButton from './NumberButton';
 //Import your array data to from the provided data file
-
-const Numbers = () => {
+import {numbers} from '../../../data';
+ const Numbers = () => {
   // STEP 2 - add the imported data to state
+const [count, doCount] = useState(numbers);
+console.log(count)
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {count.map((numbers, index) => <NumberButton numbers = {numbers} key = {index} />)} 
     </div>
   );
 };
+export default Numbers;
